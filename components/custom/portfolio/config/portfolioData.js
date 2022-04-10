065e056a5ee6b54004ec1630f5798cdc2ab9c229
chamthesleeptalker <4580789+chamthesleeptalker@users.noreply.github.com>
2022-04-10 09:58:23 +0800

    adding links to gellery

--- components/custom/portfolio/config/portfolioData.js
+++ components/custom/portfolio/config/portfolioData.js
@@ -3,37 +3,37 @@ export const portfolio = [
         src:require("@/assets/images/portfolio/img1.jpg"),
         title:"To all the Women of the World: A Data Portrait Project",
         desc:"",
-        page:"to-all-the-women-of-the-world"
+        page:"/to-all-the-women-of-the-world"
     },
     {
         src:require("@/assets/images/portfolio/img2.jpg"),
         title:"Why do we visualize data?",
         desc:"",
-        page:""
+        page:"/why-do-we-visualize-data"
     },
     {
         src:require("@/assets/images/portfolio/img3.jpg"),
         title:"Sephora Flowers",
         desc:"",
-        page:""
+        page:"/"
     },
     {
         src:require("@/assets/images/portfolio/img4.jpg"),
         title:"Trees of Innovation",
         desc:"",
-        page:""
+        page:"/"
     },
     {
         src:require("@/assets/images/portfolio/img5.jpg"),
         title:"Celebrating 5 years of Crowd2Map Tanzania",
         desc:"",
-        page:""
+        page:"/"
     },
     {
         src:require("@/assets/images/portfolio/img6.jpg"),
         title:"Drug Prevalence in Policies of the Western Pacific Region",
         desc:"",
-        page:""
+        page:"/"
     }
 ]
 
