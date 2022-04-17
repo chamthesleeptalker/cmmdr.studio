6853d704c168a99d5990d32a68d57a5838773348
chamthesleeptalker <4580789+chamthesleeptalker@users.noreply.github.com>
2022-04-17 16:57:49 +0800

    added work page

---
+++ pages/drug-policy.vue
@@ -0,0 +1,105 @@
+<template>
+  <div>
+    <Header1 />
+    <v-container>
+      <v-row class="portfolio-row mb-20 pt-20">
+        <h1 class="text-h1" style="margin-bottom: 10px">{{ title }}</h1>
+        <!--desc -->
+        <!-- <p v-html="desc"></p> -->
+        <v-container style="width: 100%">
+          <p>
+            For this one, I worked with a research team from UP Manila. 
+          </p>
+          <p>
+            Their research is on the characterization of drug policies for the countries in the Western Pacific Region. The Western Pacific Region includes Austrailia and the Pacific, Souteast Asia, and East Asia.
+          </p>
+          <p>
+            Below you can find the characterization for Australia and the Pacific.
+          </p>
+        </v-container>
+        <!-- <horizontal-scroll style="height: 60%"> -->
+          <img
+            src="@/assets/images/work/drugpolicy/AuP-Amphetamine_WDataOnly.jpg"
+            class="img-fluid about-me-img"
+            alt=""
+          />
+          <img
+            src="@/assets/images/work/drugpolicy/AuP-Cannabis_WDataOnly.jpg"
+            class="img-fluid about-me-img"
+            alt=""
+          />
+          <img
+            src="@/assets/images/work/drugpolicy/AuP-Cocaine_WDataOnly.jpg"
+            class="img-fluid about-me-img"
+            alt=""
+          />
+          <img
+            src="@/assets/images/work/drugpolicy/AuP-Ecstasy_WDataOnly.jpg"
+            class="img-fluid about-me-img"
+            alt=""
+          />
+          <img
+            src="@/assets/images/work/drugpolicy/AuP-Opiates_WDataOnly.jpg"
+            class="img-fluid about-me-img"
+            alt=""
+          />
+          <img
+            src="@/assets/images/work/drugpolicy/AuP-Opioids_WDataOnly.jpg"
+            class="img-fluid about-me-img"
+            alt=""
+          />
+          <img
+            src="@/assets/images/work/drugpolicy/AuP-Prescription_Opioids_WDataOnly.jpg"
+            class="img-fluid about-me-img"
+            alt=""
+          />
+        <!-- </horizontal-scroll> -->
+
+        <!--social -->
+
+        <!--legend -->
+        <!-- loop images -->
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+import HorizontalScroll from "vue-horizontal-scroll";
+import "vue-horizontal-scroll/dist/vue-horizontal-scroll.css";
+
+export default {
+  name: "DrugPolicy",
+  data() {
+    return {
+        title:"Drug Prevalence in Policies of the Western Pacific Region",
+        desc:"",
+        page:""
+    };
+  },
+  methods: {
+    buildImagePath: function (i) {
+      return require("@/assets/images/work/toallthewomenoftheworld/dataPortrait_" +
+        (i - 1) +
+        "_processed.jpg");
+    },
+  },
+  components: {
+    Header1: () => import("@/components/custom/header/Header1"),
+    Banner1: () => import("@/components/custom/banner/Banner1"),
+    HorizontalScroll,
+  },
+};
+</script>
+<style lang="scss" scoped>
+.portfolio-card {
+  border-radius: 0px !important;
+  box-shadow: none !important;
+}
+
+.portfolio-row {
+  margin-top: 20px;
+  margin-bottom: 20px;
+  width: 100%;
+}
+</style>
--- pages/work.vue
+++ pages/work.vue
@@ -1,22 +1,75 @@
 <template>
-  <div>
-      <Header1 />
-      <Banner1 />
+<div>
+  <Header1 />
+    <v-container>
+      <!-- -----------------------------------------------
+            Start Coming Soon
+        ----------------------------------------------- -->
+      <v-row>
+        <v-col cols="12" sm="12" md="12" class="d-flex align-center">
+          <div>
+            <h1 class="text-h1">
+              data visualization and storytelling
+            </h1>
+          </div>
+        </v-col>
+      </v-row>
+      <v-container class="portfolio-row ma-0 pt-20">
+          <v-row v-for="item in portfolio" :key="item.src" class="ma-0 pa-0">
+            <v-col cols="4" md="4" lg="4">
+                <portfolio-card :item="item"></portfolio-card>
+            </v-col>
+            <v-col cols="8" md="8" lg="8">
+              <h2 style="text-decoration:none;"><n-link class="nav-link" style="text-decoration:none;" nuxt :to="item.page">{{item.title}}</n-link></h2>
+              <p><i>{{item.date}}</i></p>
+              <br>
+              <h4>{{item.desc}}</h4>
+              <br>
+              <p>Data source: {{item.data}}</p>
+            </v-col>
+            
+          </v-row>
+      </v-container>
+      <!-- -----------------------------------------------
+            End Coming Soon
+        ----------------------------------------------- -->
+    </v-container>
   </div>
 </template>
-
 <script>
-import { mapGetters } from "vuex";
-export default {
-  name: "work",
-  data() {
-    return {};
-  },
+import { portfolio } from "@/components/custom/portfolio/config/portfolioData";
 
+export default {
+  name: "Work",
+  data() {
+    return {
+      portfolio,
+      reveal: false,
+    };
+  },
   components: {
+    PortfolioCard: () => import("@/components/custom/portfolio/PortfolioCard"),
     Header1: () => import("@/components/custom/header/Header1"),
-    // ComingSoon: () => import("@/components/shared/coming-soon/ComingSoon"),
     Banner1: () => import("@/components/custom/banner/Banner1"),
   },
+  methods: {},
+  mounted(){
+    console.log(this.portfolio);
+  }
 };
 </script>
+<style lang="scss">
+.portfolio-component {
+  padding:0px;
+  margin:0px;
+}
+
+.portfolio-card {
+  border-radius:0px !important;
+  box-shadow:none !important;
+}
+
+.portfolio-row{
+  width:100%;
+}
+</style>
