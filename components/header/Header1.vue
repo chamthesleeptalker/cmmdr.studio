d8c53e67c6045362ba34aaf5f50486c64e2ce9fd
chamthesleeptalker <4580789+chamthesleeptalker@users.noreply.github.com>
2023-11-05 23:00:32 +0800

    added design process

--- components/header/Header1.vue
+++ components/header/Header1.vue
@@ -3,11 +3,7 @@
     <!-- -----------------------------------------------
           Start Header
     ----------------------------------------------- -->
-    <v-app-bar
-      app
-      class="app-header position-relative navbar-light header1"
-      flat
-    >
+    <v-app-bar app class="app-header position-relative navbar-light header1" flat>
       <v-container class="py-0 fill-height">
         <!-- Logo -->
         <!-- <div class="logo">
@@ -24,17 +20,26 @@
           <v-app-bar-nav-icon />
         </v-btn>
         <!-- Desktop view Navigation -->
-        <div
-          class="navigation"
-          v-bind:class="[isActive ? 'd-block' : '']"
-          @click="isActive = !isActive"
-        >
+        <div class="navigation" v-bind:class="[isActive ? 'd-block' : '']" @click="isActive = !isActive">
           <ul class="navbar-nav">
             <li class="nav-item" text>
               <n-link class="nav-link" nuxt to="/"> Home </n-link>
             </li>
             <li class="nav-item" text>
               <n-link class="nav-link" nuxt to="/about-me"> About Me </n-link>
+            </li>
+            <li class="nav-item" text>
+              <n-link id="menu-activator" class="nav-link" nuxt to="/how-do-i-make"> My process </n-link>
+              <v-menu id="menu-my-process" activator="#menu-activator" open-on-hover location="end bottom" origin="top end">
+                <v-list>
+                  <v-list-item  text>
+                    <n-link nuxt to="/how-do-i-make#how-might-we" style="text-decoration: none;">Design thinking</n-link>
+                  </v-list-item>
+                  <v-list-item text>
+                    <n-link nuxt to="/how-do-i-make#craft-data-viz-story" style="text-decoration: none;">Crafting impactful data viz & story</n-link>
+                  </v-list-item>
+                </v-list>
+              </v-menu>
             </li>
             <li class="nav-item" text>
               <n-link class="nav-link" nuxt to="/work"> Projects </n-link>
@@ -87,3 +92,13 @@ export default {
   },
 };
 </script>
+<style lang="scss">
+.blog-img {
+  height: 250px;
+}
+
+#menu-my-process {
+  top: 70px;
+}
+</style>
+
---
+++ pages/how-do-i-make.vue
@@ -0,0 +1,294 @@
+<template>
+  <div>
+    <Header1 />
+    <v-container class="mt-5">
+      <v-row>
+        <v-container>
+          <v-row id="how-might-we">
+            <h1 class="mt-5">
+              Design thinking, a framework to innovation
+            </h1>
+            <br>
+          </v-row>
+          <v-row class="mt-10">
+            <p>
+              To put a structure in my works, I rely in the iterative nature of design thinking. Design thinking is an
+              innovation tool that follows a 5-step process which are Empathize, Define, Ideate, Prototype, and Test. In
+              my experience, it’s a mostly linear process with a chance of hopping back to the previous steps based on the
+              outcome of each step. This is because as you move towards the next step you will learn more about your users
+              and the problem they want solved which may lead you to reassess your current prototype, the identified core
+              problem, or add more dimension to your user persona.
+            </p>
+          </v-row>
+          <v-row>
+            <v-card class="pa-5" flat>
+              <div class="d-flex flex-no-wrap justify-space-between">
+                <v-avatar class="ma-5" size="125" rounded="0">
+                  <v-img :src="require('@/assets/images/how-do-i-make/1.png')"></v-img>
+                </v-avatar>
+                <div>
+                  <v-card-title class="text-h5">
+                    Empathize
+                  </v-card-title>
+                  <v-card-subtitle class="pt-5">
+                    <p>
+                      This step is what makes coming up with a human-centered solution possible. Here you will gather
+                      artefacts about your users of your product or the intended audience for your output. Artefacts can
+                      be
+                      collected through user interviews, surveys, focus groups, video/voice diaries, walkthroughs,
+                      what-how-whys, and others.
+                    </p>
+                    <p>
+                      This stage will help us to come up with a solid persona for which every decision moving forward will
+                      be
+                      based on. It helps if it has a name.
+                    </p>
+                  </v-card-subtitle>
+                </div>
+              </div>
+            </v-card>
+            <v-card class="pa-5" flat>
+              <div class="d-flex flex-no-wrap justify-space-between">
+                <v-avatar class="ma-5" size="125" rounded="0">
+                  <v-img :src="require('@/assets/images/how-do-i-make/2.png')"></v-img>
+                </v-avatar>
+                <div>
+                  <v-card-title class="text-h5">
+                    Define
+                  </v-card-title>
+                  <v-card-subtitle class="pt-5">
+                    <p>
+                      The empathize stage should give us the user persona that we will work with in defining the core problem that we will design a solution for. This will come largely from the list of pain points that we have uncovered during the empathize stage. Through techniques such as dot voting, 5-whys, why-how laddering, and so on.
+                    </p>
+                  </v-card-subtitle>
+                </div>
+              </div>
+            </v-card>
+            <v-card class="pa-5" flat>
+              <div class="d-flex flex-no-wrap justify-space-between">
+                <v-avatar class="ma-5" size="125" rounded="0">
+                  <v-img :src="require('@/assets/images/how-do-i-make/3.png')"></v-img>
+                </v-avatar>
+                <div>
+                  <v-card-title class="text-h5">
+                    Ideate
+                  </v-card-title>
+                  <v-card-subtitle class="pt-5">
+                    <p>
+                      A solution needs to be useful, usable, and desirable. In this step is where we think about numerous solutions to the core problem you have identified in the Define stage. In my experience, anything goes! You will get quirky and really out-there ideas. Some, albeit being impossible to execute, helps to get more ideas to the table. I really enjoy this part of the design thinking process where you can make full use of experiences and knowledge by shaping it with your imagination and creativity.
+Here I employ a diverge-converge approach. Brainstorm a number of ideas, merge to converge analogous or related ideas to few key ideas, evaluate feasibility and viability of the remaining selections, and prioritize according to available resources.
+                    </p>
+                  </v-card-subtitle>
+                </div>
+              </div>
+            </v-card>
+            <v-card class="pa-5" flat>
+              <div class="d-flex flex-no-wrap justify-space-between">
+                <v-avatar class="ma-5" size="125" rounded="0">
+                  <v-img :src="require('@/assets/images/how-do-i-make/4.png')"></v-img>
+                </v-avatar>
+                <div>
+                  <v-card-title class="text-h5">
+                    Prototype
+                  </v-card-title>
+                  <v-card-subtitle class="pt-5">
+                    <p>
+                      Time to get our hands dirty (can be figuratively or literally if the solution is somehow related to agriculture)! Prototypes can take various shapes and form. I like to view prototypes as our primary tool to validate your assumptions about how to the solution you came up with in the previous stage. It’s a bundle of assumptions about how the problem can be solved. Assumptions about a solution to solve the core problem defined through persona created in the two previous stages.
+                    </p>
+                    <p>
+                      In my experience, as long as your assumptions can be validated with your users in the next stage, Test, the best prototype is cheap and low effort. 
+                    </p>
+                  </v-card-subtitle>
+                </div>
+              </div>
+            </v-card>
+            <v-card class="pa-5" flat>
+              <div class="d-flex flex-no-wrap justify-space-between">
+                <v-avatar class="ma-5" size="125" rounded="0">
+                  <v-img :src="require('@/assets/images/how-do-i-make/5.png')"></v-img>
+                </v-avatar>
+                <div>
+                  <v-card-title class="text-h5">
+                    Test
+                  </v-card-title>
+                  <v-card-subtitle class="pt-5">
+                    <p>
+                      This time we will put our prototype to work. In my work I employ, at least 3 levels of tests before testing with actual users as often reaching users will incur some cost. First level is  test it with a a few from my team or colleagues within the project. Get their thoughts and get things fixed based on their feedback. Test it to 2 more groups within your company/network (if the data is not sensitive or the project is open sourced) then go to your users 😊 By this time, your prototype may look a bit or a lot different from when you started. Learn from your users and go back to the assumptions that you made in the prototype stage and check if they are validated or if they rejected. 
+                    </p>
+                  </v-card-subtitle>
+                </div>
+              </div>
+            </v-card>
+          </v-row>
+          <v-divider class="mt-10 mt-10"></v-divider>
+          <v-row id="craft-data-viz-story" class="mt-10 mb-10">
+            <h1 class="mt-5 mb-5">
+              The value of communicating impact
+            </h1>
+            <p>
+              My projects throughout the years helped me to develop an awareness of how good design can make a difference. Design is universal. You can design an app, show, video, sound, illustration, stories, sculpture, furniture, cars, rockets and everything! But only great design can deliver the impact that you maybe seeking. 
+            </p>
+            <p>
+              And another thing that we can design is information. That’s right, it’s information design.
+To be honest, I’m not aware that there is a formal domain of study for how to effectively present information to maximize comprehension and impact. However, being a geomatics engineer by education and practice, I have already dabbled with information design a lot through cartography, the science and art of making maps! 
+            </p>
+            <p>
+              I never thought about it before but maps are the most efficient way to present information wherein the spatial location is a relevant data dimension to be displayed. We we’re working with big data before it gained popularity in the tech space. Yes, we did! Satellite images and cloud point datasets. We just know that our codes needs to be design optimally to avoid longer-than-we-can-wait runtimes. 
+            </p>
+            <p>
+              For me, working with maps, naturally evolved into making mapping applications as often a static map limited in terms of the layers of information you can display. With more information available, the next thing to think about is how to display it together with your map. This is where I started to really lean into learning about visual design, interaction design, user experience design, Gestalt principles, and cognitive psychology.
+            </p>
+            <p>
+              In making a data visualization or story, I follow this 5-step recipe I’ve used effectively in similar projects. 
+            </p>
+          </v-row>
+          
+          <v-row class="mt-5">
+            <h3>
+              My 5-step Guide to an Impactful Viz & Story
+            </h3>
+            <v-card flat>
+              <div class="d-flex flex-no-wrap justify-space-between">
+                <v-avatar class="ma-5" size="125" rounded="0">
+                  <v-img :src="require('@/assets/images/how-do-i-make/10.png')"></v-img>
+                </v-avatar>
+                <div>
+                  <v-card-title class="text-h5">
+                    Establish the data story’s purpose
+                  </v-card-title>
+                  <v-card-subtitle class="pt-5">
+                    <p>
+                      Who is the story for? When will it be published? What’s the purpose of the story? Is it to inform? Is it to present an argument? The purpose of the story defines the needed data/evidence, tone of storytelling, and overall aesthetic.
+                    </p>
+                  </v-card-subtitle>
+                </div>
+              </div>
+            </v-card>
+            <v-card flat>
+              <div class="d-flex flex-no-wrap justify-space-between">
+                <v-avatar class="ma-5" size="125" rounded="0">
+                  <v-img :src="require('@/assets/images/how-do-i-make/11.png')"></v-img>
+                </v-avatar>
+                <div>
+                  <v-card-title class="text-h5">
+                    Gather data, validate data sources & methodologies
+                  </v-card-title>
+                  <v-card-subtitle class="pt-5">
+                    <p>
+                      Often, you may have a dataset to work with already. If that’s not the case, Check out this list of possible data sources. Study your dataset and the methodology employed to create it. Take note of assumptions and notes about the datasets.
+                    </p>
+                  </v-card-subtitle>
+                </div>
+              </div>
+            </v-card>
+            <v-card flat>
+              <div class="d-flex flex-no-wrap justify-space-between">
+                <v-avatar class="ma-5" size="125" rounded="0">
+                  <v-img :src="require('@/assets/images/how-do-i-make/12.png')"></v-img>
+                </v-avatar>
+                <div>
+                  <v-card-title class="text-h5">
+                    Explore your datasets & do adjacent research
+                  </v-card-title>
+                  <v-card-subtitle class="pt-5">
+                    <p>
+                      Choose a data exploration tool and explore the data. It can be your favorite spreadsheet software and create a couple of charts/graphs/maps. Be on the watch for patterns, outliers, missing data and so on. Create an informed perspective by reading articles and papers, watching videos, and so on.
+                    </p>
+                  </v-card-subtitle>
+                </div>
+              </div>
+            </v-card>
+            <v-card flat>
+              <div class="d-flex flex-no-wrap justify-space-between">
+                <v-avatar class="ma-5" size="125" rounded="0">
+                  <v-img :src="require('@/assets/images/how-do-i-make/13.png')"></v-img>
+                </v-avatar>
+                <div>
+                  <v-card-title class="text-h5">
+                    Structure your story around data & its purpose
+                  </v-card-title>
+                  <v-card-subtitle class="pt-5">
+                    <p>
+                      Outline your data story. At the minimum, it needs to have an INTRO, ARGUMENT/BODY, and a RESOLUTION. In between, there should be a transition element to ensure that the flow of thoughts is smooth. Every element in your data story should contribute to its purpose. This should help your story be focused and concise.
+                    </p>
+                  </v-card-subtitle>
+                </div>
+              </div>
+            </v-card>
+            <v-card flat>
+              <div class="d-flex flex-no-wrap justify-space-between">
+                <v-avatar class="ma-5" size="125" rounded="0">
+                  <v-img :src="require('@/assets/images/how-do-i-make/14.png')"></v-img>
+                </v-avatar>
+                <div>
+                  <v-card-title class="text-h5">
+                    Put it all together, test & iterate.
+                  </v-card-title>
+                  <v-card-subtitle class="pt-5">
+                    <p>
+                      Put your story together. Support text with visuals. It can be a visualization or an image. Make it interactive. I believe that data stories are more effective if the story is uncovered by the readers/users themselves.
+                    </p>
+                    <p>
+                      Emphasize and highlight crucial info to boost comprehension on the most important bits. Make it fun :)
+                    </p>
+                  </v-card-subtitle>
+                </div>
+              </div>
+            </v-card>
+          </v-row>
+        </v-container>
+      </v-row>
+    </v-container>
+    <br>
+  </div>
+</template>
+
+<script>
+import { awards } from "@/data/resume";
+import { aboutWorkItems, aboutSummary, aboutEducation, aboutCategories, aboutPublication, aboutSpeaking } from "@/data/about";
+export default {
+  name: "how-do-i-make",
+  data() {
+    return {
+      awards,
+      aboutWorkItems,
+      aboutSummary,
+      aboutEducation,
+      activeCategory: "dataViz",
+      aboutCategories,
+      aboutPublication,
+      aboutSpeaking,
+      toggle: null,
+      items: [
+        {
+          title: 'Education',
+          value: "#education",
+        },
+        {
+          title: 'Publications',
+          value: "#publications",
+        },
+        {
+          title: 'Speaking Engagements',
+          value: "#speaking",
+        },
+      ],
+    };
+  },
+
+  components: {
+    Header1: () => import("@/components/header/Header1"),
+    Banner1: () => import("@/components/banner/Banner1"),
+  },
+  mounted() {
+    this.activeCategory = 'dataViz';
+  },
+};
+</script>
+<style lang="scss" scoped>.about-me-img {
+  max-width: 70%;
+}
+
+.p-full-line {
+  width: 100%;
+}</style>
